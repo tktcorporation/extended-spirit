d2320ff791c2c05ccf011987118def2cf0c5dcc2
tktcorporation <tktcorporation.go@gmail.com>
2021-03-13 21:17:44 +0900

    :+1: success to get project data

--- app/src/event.ts
+++ app/src/event.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 enum MenuId {
   GetOrgs,
-  GetAuthToken,
+  GetAccessToken,
+  GetProject,
 }
 
 chrome.runtime.onInstalled.addListener(() => {
@@ -16,9 +17,14 @@ chrome.runtime.onInstalled.addListener(() => {
     title: 'get orgs',
   });
   chrome.contextMenus.create({
-    id: MenuId.GetAuthToken.toString(),
+    id: MenuId.GetAccessToken.toString(),
     parentId: 'parent',
     title: 'get auth token',
+  });
+  chrome.contextMenus.create({
+    id: MenuId.GetProject.toString(),
+    parentId: 'parent',
+    title: 'get project',
   });
 });
 
@@ -30,14 +36,11 @@ chrome.contextMenus.onClicked.addListener(async (item) => {
     switch (item.menuItemId) {
       case MenuId.GetOrgs.toString():
         alert(await getOrganizationsData());
-      case MenuId.GetAuthToken.toString():
-        await chrome.cookies.getAll(
-          { domain: 'app.holaspirit.com', name: 'holaAppToken' },
-          async (cookie) => {
-            console.log(`cccccc: ${cookie}`);
-            alert(await getAuthToken(cookie));
-          },
-        );
+      case MenuId.GetAccessToken.toString():
+        alert(await getAccessToken());
+      case MenuId.GetProject.toString():
+        const accessToken = await getAccessToken();
+        alert(await getProject(accessToken, '6048054642d878641215a440'));
     }
   } catch (error) {
     alert(error.message);
@@ -51,10 +54,37 @@ const getOrganizationsData = async () => {
   return JSON.stringify(response.data);
 };
 
-const getAuthToken = (AuthTokenCookie: chrome.cookies.Cookie[]) => {
-  if (AuthTokenCookie.length < 1) {
+const getProject = async (
+  accessToken: string,
+  projectId: string,
+  organizationId = '6048000e343b9430fb74ecf9',
+) => {
+  const client = axios.create({
+    baseURL: 'https://app.holaspirit.com',
+    headers: {
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${accessToken}`,
+    },
+    responseType: 'json',
+  });
+  const response = await client.get(
+    `/api/organizations/${organizationId}/projects/${projectId}`,
+  );
+  return JSON.stringify(response.data);
+};
+
+const getAccessToken = async (): Promise<string> => {
+  const cookies = await new Promise<chrome.cookies.Cookie[]>((resolve) => {
+    chrome.cookies.getAll(
+      { domain: 'app.holaspirit.com', name: 'holaAppToken' },
+      (cookie) => {
+        resolve(cookie);
+      },
+    );
+  });
+  if (cookies.length < 1) {
     throw Error('認証トークンが見つかりませんでした');
   }
-  const holaAppTokenEncoded = AuthTokenCookie[0].value;
+  const holaAppTokenEncoded = cookies[0].value;
   return JSON.parse(decodeURIComponent(holaAppTokenEncoded)).access_token;
 };
