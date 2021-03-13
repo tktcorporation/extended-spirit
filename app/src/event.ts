a30005eec6bde30254a503cc9f328217800c87ca
tktcorporation <tktcorporation.go@gmail.com>
2021-03-13 19:59:31 +0900

    :+1: success to get the auth token

--- app/src/event.ts
+++ app/src/event.ts
@@ -1,38 +1,60 @@
-// Extension event listeners are a little different from the patterns you may have seen in DOM or
-// Node.js APIs. The below event listener registration can be broken in to 4 distinct parts:
-//
-// * chrome      - the global namespace for Chrome's extension APIs
-// * runtime     – the namespace of the specific API we want to use
-// * onInstalled - the event we want to subscribe to
-// * addListener - what we want to do with this event
-//
-
 import axios from 'axios';
 
-// See https://developer.chrome.com/docs/extensions/reference/events/ for additional details.
-chrome.runtime.onInstalled.addListener(async () => {
-    // While we could have used `let url = "hello.html"`, using runtime.getURL is a bit more robust as
-    // it returns a full URL rather than just a path that Chrome needs to be resolved contextually at
-    // runtime.
-    const url = chrome.runtime.getURL('hello.html');
+enum MenuId {
+    GetOrgs,
+    GetAuthToken,
+}
 
-    // Open a new tab pointing at our page's URL using JavaScript's object initializer shorthand.
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#new_notations_in_ecmascript_2015
-    //
-    // Many of the extension platform's APIs are asynchronous and can either take a callback argument
-    // or return a promise. Since we're inside an async function, we can await the resolution of the
-    // promise returned by the tabs.create call. See the following link for more info on async/await.
-    // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Async_await
-    await chrome.tabs.create({ url }, async (tab) => {
-        // Finally, let's log the ID of the newly created tab using a template literal.
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-        //
-        // To view this log message, open chrome://extensions, find "Hello, World!", and click the
-        // "service worker" link in th card to open DevTools.
-        console.log(`Created tab ${tab.id}`);
-        const response = await axios.get(
-            'https://app.holaspirit.com/api/public/organizations',
-        );
-        window.alert(response.status);
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: 'parent',
+        title: 'holaspirit',
+    });
+    chrome.contextMenus.create({
+        id: MenuId.GetOrgs.toString(),
+        parentId: 'parent',
+        title: 'get orgs',
+    });
+    chrome.contextMenus.create({
+        id: MenuId.GetAuthToken.toString(),
+        parentId: 'parent',
+        title: 'get auth token',
     });
 });
+
+// メニューをクリック時に実行
+chrome.contextMenus.onClicked.addListener(async (item) => {
+    console.log(item);
+    console.log(item.menuItemId);
+    try {
+        switch (item.menuItemId) {
+            case MenuId.GetOrgs.toString():
+                alert(await getOrganizationsData());
+            case MenuId.GetAuthToken.toString():
+                await chrome.cookies.getAll(
+                    { domain: 'app.holaspirit.com', name: 'holaAppToken' },
+                    async (cookie) => {
+                        console.log(`cccccc: ${cookie}`);
+                        alert(await getAuthToken(cookie));
+                    },
+                );
+        }
+    } catch (error) {
+        alert(error.message);
+    }
+});
+
+const getOrganizationsData = async () => {
+    const response = await axios.get(
+        'https://app.holaspirit.com/api/public/organizations',
+    );
+    return JSON.stringify(response.data);
+};
+
+const getAuthToken = (AuthTokenCookie: chrome.cookies.Cookie[]) => {
+    if (AuthTokenCookie.length < 1) {
+        throw Error('認証トークンが見つかりませんでした');
+    }
+    const holaAppTokenEncoded = AuthTokenCookie[0].value;
+    return JSON.parse(decodeURIComponent(holaAppTokenEncoded)).access_token;
+};
