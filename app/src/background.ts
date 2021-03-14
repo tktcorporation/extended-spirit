40f896e7b322390d5d14de5d39d456ad14474ea0
tktcorporation <tktcorporation.go@gmail.com>
2021-03-14 15:13:15 +0900

    :recycle: Change indent size

--- app/src/background.ts
+++ app/src/background.ts
@@ -8,24 +8,24 @@
 //
 // See https://developer.chrome.com/docs/extensions/reference/events/ for additional details.
 chrome.runtime.onInstalled.addListener(async () => {
-    // While we could have used `let url = "hello.html"`, using runtime.getURL is a bit more robust as
-    // it returns a full URL rather than just a path that Chrome needs to be resolved contextually at
-    // runtime.
-    const url = chrome.runtime.getURL('hello.html');
+  // While we could have used `let url = "hello.html"`, using runtime.getURL is a bit more robust as
+  // it returns a full URL rather than just a path that Chrome needs to be resolved contextually at
+  // runtime.
+  const url = chrome.runtime.getURL('hello.html');
 
-    // Open a new tab pointing at our page's URL using JavaScript's object initializer shorthand.
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#new_notations_in_ecmascript_2015
+  // Open a new tab pointing at our page's URL using JavaScript's object initializer shorthand.
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#new_notations_in_ecmascript_2015
+  //
+  // Many of the extension platform's APIs are asynchronous and can either take a callback argument
+  // or return a promise. Since we're inside an async function, we can await the resolution of the
+  // promise returned by the tabs.create call. See the following link for more info on async/await.
+  // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Async_await
+  await chrome.tabs.create({ url }, (tab) => {
+    // Finally, let's log the ID of the newly created tab using a template literal.
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
     //
-    // Many of the extension platform's APIs are asynchronous and can either take a callback argument
-    // or return a promise. Since we're inside an async function, we can await the resolution of the
-    // promise returned by the tabs.create call. See the following link for more info on async/await.
-    // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Async_await
-    await chrome.tabs.create({ url }, (tab) => {
-        // Finally, let's log the ID of the newly created tab using a template literal.
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-        //
-        // To view this log message, open chrome://extensions, find "Hello, World!", and click the
-        // "service worker" link in th card to open DevTools.
-        console.log(`Created tab ${tab.id}`);
-    });
+    // To view this log message, open chrome://extensions, find "Hello, World!", and click the
+    // "service worker" link in th card to open DevTools.
+    console.log(`Created tab ${tab.id}`);
+  });
 });
